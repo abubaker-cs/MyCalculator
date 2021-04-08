c7ffea2a088ef9b359b0129bb8503e65651a3b00
abubaker <mohammad_abubaker@yahoo.com>
2021-04-08 10:23:27 +0500

    base setup

---
+++ app/src/main/java/org/abubaker/mycalculator/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.abubaker.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.mycalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // It will load activity_main.xml view
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+
+}
